121758108629e315c3c17f130c6e0b1047f710cc
rhwlgns4386 <rhwlgns4386@naver.com>
2025-08-20 12:08:20 +0900

    style : 필요없는 의존성 제거

--- src/main/java/com/hunko/algostack/algorithm/domain/vo/AlgorithmReadModel.java
+++ src/main/java/com/hunko/algostack/algorithm/domain/vo/AlgorithmReadModel.java
@@ -4,7 +4,6 @@ import com.hunko.algostack.algorithm.domain.entity.Platform;
 import com.hunko.algostack.algorithm.domain.entity.Result;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 public record AlgorithmReadModel(
         Platform platform,
